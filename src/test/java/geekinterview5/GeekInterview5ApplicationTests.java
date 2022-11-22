bb2cbe90b7bd472ca41c78f7ebef1e6eb649e943
celean77777 <celean77777@gmail.com>
2022-11-22 21:50:27 +0300

    Initial commit

---
+++ src/test/java/geekinterview5/GeekInterview5ApplicationTests.java
@@ -0,0 +1,13 @@
+package geekinterview5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekInterview5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
